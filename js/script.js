88aba95bac5b50bb75ac3ec494f8975d29a1537e
Mirosław Lewandowski <miroslaw.leszek.lewandowski@gmail.com>
2021-04-27 10:34:18 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Cześć!");
+
+let removeHeader = document.querySelector(".js-removeHeader");
+let body__header = document.querySelector(".body__header");
+
+removeHeader.addEventListener("click", () => {
+
+    body__header.remove();
+});
+
+
+let button = document.querySelector(".js-changeBackground");
+let body = document.querySelector(".body");
+let button__jsthemeName = document.querySelector(".button__jsthemeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("button__whiteBackground");
+
+    button__jsthemeName.innerText = body.classList.contains("button__whiteBackground") ? "szare" : "białe";
+});
