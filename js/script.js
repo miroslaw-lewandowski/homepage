2645eff88135e4b62bd029c45d5c51a24a6b7a5b
Mirosław Lewandowski <miroslaw.leszek.lewandowski@gmail.com>
2021-05-04 01:02:40 +0200

    added functions to js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
-function welcome() {
-    console.log("Cześć! Witam na mojej pierwszej stronie");
+{
+    const welcome = () => {
+        console.log("Cześć! Witam na mojej pierwszej stronie");
+    }
+
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const button__jsthemeName = document.querySelector(".button__jsthemeName");
+        body.classList.toggle("button__whiteBackground");
+        button__jsthemeName.innerText = body.classList.contains("button__whiteBackground") ? "szare" : "białe";
+    };
+
+    const deleteHeader = () => {
+        const body__header = document.querySelector(".body__header");
+        body__header.remove();
+    };
+
+    const init = () => {
+        const button = document.querySelector(".js-changeBackground");
+        button.addEventListener("click", toggleBackground);
+        const removeHeader = document.querySelector(".js-removeHeader");
+        removeHeader.addEventListener("click", deleteHeader);
+
+        welcome();
+    };
+
+    init();
 }
 
-welcome();
 
 
-let removeHeader = document.querySelector(".js-removeHeader");
-let body__header = document.querySelector(".body__header");
-
-removeHeader.addEventListener("click", () => {
-
-    body__header.remove();
-});
 
 
-let button = document.querySelector(".js-changeBackground");
-let body = document.querySelector(".body");
-let button__jsthemeName = document.querySelector(".button__jsthemeName");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("button__whiteBackground");
 
-    button__jsthemeName.innerText = body.classList.contains("button__whiteBackground") ? "szare" : "białe";
-});
+
+
+
+
+
+
+
+
+
+
+
