bffa1a93d60a5431256920cbf66165117dfe93c9
Mirosław Lewandowski <miroslaw.leszek.lewandowski@gmail.com>
2021-05-12 23:24:26 +0200

    class names changed in html, css, js

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@
 
     const toggleBackground = () => {
         const body = document.querySelector(".body");
-        const button__jsthemeName = document.querySelector(".button__jsthemeName");
-        body.classList.toggle("button__whiteBackground");
-        button__jsthemeName.innerText = body.classList.contains("button__whiteBackground") ? "szare" : "białe";
+        const buttonText = document.querySelector(".js-button__text");
+        body.classList.toggle("body__whiteBackground");
+        buttonText.innerText = body.classList.contains("body__whiteBackground") ? "szare" : "białe";
     };
 
     const deleteHeader = () => {
-        const body__header = document.querySelector(".body__header");
-        body__header.remove();
+        const bodyHeader = document.querySelector(".js-body__header");
+        bodyHeader.remove();
     };
 
     const init = () => {
-        const button = document.querySelector(".js-changeBackground");
-        button.addEventListener("click", toggleBackground);
-        const removeHeader = document.querySelector(".js-removeHeader");
-        removeHeader.addEventListener("click", deleteHeader);
+        const changeBackgroundButton = document.querySelector(".js-changeBackground");
+        changeBackgroundButton.addEventListener("click", toggleBackground);
+        const removeButton = document.querySelector(".js-removeHeader");
+        removeButton.addEventListener("click", deleteHeader);
 
         welcome();
     };
